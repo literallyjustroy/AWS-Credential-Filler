cc85f60c6c992b8d0385b8fb4de88f5b3ac0d958
Roy Smith <rsmith@veracode.com>
2022-05-21 21:15:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# AWS Landing Zone Creds Fill
+
+This project:
+1. Navigates to a given AWS Landing zone
+2. Signs in with the provided USERNAME and PASSWORD(using MFA passed as an argument or on the clipboard)
+3. Copies the CLI credentials from the first account in the list to the clipboard
+4. Saves them as the \[default\] account in the users ~/.aws/credentials file
+5. Clears the clipboard
+
+## Usage
+
+Copy your MFA code to your clipboard (or paste it as an optional first argument) and run the following command:
+```shell
+npm run start
+```
+
+I'd also recommend binding this to an alias. For example, add the following to your `~/.zshrc` file:
+```shell
+alias creds="node ~/dev/auto/creds/index.js"
+```
+
+This allows you to run `creds` from anywhere
---
+++ index.js
@@ -0,0 +1,94 @@
+import {createRequire} from "module";
+
+const require = createRequire(import.meta.url);
+
+require('dotenv').config()
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+const os = require('os');
+const path = require('path');
+import clipboardy from 'clipboardy';
+
+if (!process.env.URL || !process.env.USERNAME || !process.env.PASSWORD) {
+    console.error('Fill in the .env file with your AWS Landing zone URL, USERNAME, and PASSWORD')
+    process.exit(1);
+}
+
+let mfa;
+const args = process.argv.slice(2);
+
+if (args.length === 1) {
+    mfa = args[0];
+} else {
+    mfa = clipboardy.readSync();
+    if (mfa && mfa.length === 6 && mfa.match('[0-9]{6}')) {
+        console.log('MFA acquired from clipboard: ' + mfa);
+    } else {
+        console.error("Requires one argument of the MFA code or MFA saved to clipboard.");
+        process.exit(1);
+    }
+}
+
+console.log('Setting AWS Credentials...');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions(process.env.URL.origin, ['clipboard-read'])
+
+    const page = await browser.newPage();
+    await page.goto(process.env.URL);
+
+    await page.waitForSelector("#username-input");
+    await page.keyboard.type(process.env.USERNAME);
+    await page.click("#username-submit-button");
+
+    await page.waitForSelector("#password-input");
+    await page.keyboard.type(process.env.PASSWORD);
+    await page.click("#password-submit-button");
+
+    await page.waitForSelector("div[data-testid='vmfa-authentication']");
+    await page.keyboard.type(mfa);
+    await page.click("button[type='submit']");
+
+    try {
+        await page.waitForNavigation({timeout: 3000}); // Wait MFA navigation
+    } catch (e) {
+        console.error(e);
+        console.error('\nError: Page failed to navigate after MFA input. Likely MFA code timed-out.')
+        process.exit(1);
+    }
+
+    console.log('MFA Accepted')
+
+    await page.waitForSelector("portal-application");
+    await page.click("portal-application");
+
+    await page.waitForSelector(".instance-block");
+    await page.click(".instance-block");
+
+    await page.waitForTimeout(2000);
+
+    await page.waitForSelector("#temp-credentials-button");
+    await page.click("#temp-credentials-button");
+
+    await page.waitForTimeout(2000);
+
+    await page.waitForSelector("#cli-cred-file-code");
+    await page.click("#cli-cred-file-code"); // Copy creds to clipboard
+
+    const rawCredsText = await page.evaluate(() => navigator.clipboard.readText());
+
+    await browser.close();
+
+    const credsLines = rawCredsText.split('\n')
+    const accountName = credsLines[0].match(/\[(.*)]/)[1] // AWS-Account-Engineer
+
+    // Replace the account name with default
+    const credsText = ['[default]', ...credsLines.slice(1)].join('\n')
+
+    fs.writeFileSync(path.join(os.homedir(), '.aws/credentials'), credsText, {flag: 'w'})
+    console.log('Credentials file updated for account: ' + accountName)
+
+    clipboardy.writeSync(''); // Clear clipboard
+})();
